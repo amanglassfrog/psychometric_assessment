5f1f99b5bf7204ecd4184d19c34b76ac27af4a4c
Shubham Kumar <shubham@glassfrog.design>
2024-05-28 12:47:18 +0530

    changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -225,7 +225,7 @@ const Header = () => {
                 </div>
             </header>
             <section class="bg-white dark:bg-gray-900 min-h-[91vh] flex items-center">
-                <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+                <div class="grid max-w-screen-xl px-4  py-32 md:py-16 sm:py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                     <div class="mr-auto place-self-center lg:col-span-7">
                         <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">AI powered talent
                             assessment platform</h1>
@@ -322,7 +322,7 @@ const Header = () => {
 
                         </div>
                     </div>
-                    <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                    <div class=" lg:mt-0 lg:col-span-5 lg:flex">
                         <img src="/hero.png" alt="mockup" />
                     </div>
                 </div>
