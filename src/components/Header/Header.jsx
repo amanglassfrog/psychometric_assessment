f75f0136de132f85c8bf4f93aeb78091eafb33c7
Shubham Kumar <shubham@glassfrog.design>
2024-05-28 13:49:03 +0530

    dark removed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -224,12 +224,12 @@ const Header = () => {
                     </nav>
                 </div>
             </header>
-            <section class="bg-white dark:bg-gray-900 min-h-[91vh] flex items-center">
+            <section class="bg-white  min-h-[91vh] flex items-center">
                 <div class="grid max-w-screen-xl px-4  py-32 md:py-16 sm:py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                     <div class="mr-auto place-self-center lg:col-span-7">
-                        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">AI powered talent
+                        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl ">AI powered talent
                             assessment platform</h1>
-                        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Recruiting is complex, time-consuming, and risky. Take the stress out of finding the best candidates with deep analysis that’s accurate, automated, and unbiased. Make hiring exciting again.</p>
+                        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl ">Recruiting is complex, time-consuming, and risky. Take the stress out of finding the best candidates with deep analysis that’s accurate, automated, and unbiased. Make hiring exciting again.</p>
                         <a onClick={openModal} href="#" class="bad inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center  ">
                             Book a demo                            <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </a>
@@ -314,7 +314,7 @@ const Header = () => {
                                 </div>
                             </div>
                         )}
-                        <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                        <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100  ">
                             Try for free                        </a>
                         <div className='pt-5 flex gap-2'>
                             <span className='flex items-center gap-2 line'><svg className='icon1' viewBox="0 0 576 512" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path d="M0 432c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V256H0v176zm192-68c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H204c-6.6 0-12-5.4-12-12v-40zm-128 0c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM576 80v48H0V80c0-26.5 21.5-48 48-48h480c26.5 0 48 21.5 48 48z"></path></svg><b>No credit card required</b></span>
