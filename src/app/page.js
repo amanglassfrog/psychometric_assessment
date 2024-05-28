bb9800b6e402d2e3b583c7196667f7a7e3f51617
amanglassfrog <135016564+amanglassfrog@users.noreply.github.com>
2024-05-28 21:22:18 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
+    <head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-T1RMK1QQK9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-T1RMK1QQK9');
+</script>
+    </head>
     <Header />
+    </>
   );
 }
