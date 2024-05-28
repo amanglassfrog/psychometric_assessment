3a671fea7ad1ee619110a935aeafa8213785497c
amanglassfrog <135016564+amanglassfrog@users.noreply.github.com>
2024-05-28 21:31:26 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,26 @@
 import Header from "@/components/Header/Header";
-import Head from "next/head";
-import Script from "next/script";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Glassfrog Academy</title>
-      </Head>
-      <Script
+      <Header />
+    <head>
+      <script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-T1RMK1QQK9"
-      ></Script>
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-          gtag('config', 'G-T1RMK1QQK9');
-        `}
-      </Script>
-      <Header />
+            gtag('config', 'G-T1RMK1QQK9');
+          `,
+        }}
+      ></script>
+        </head>
     </>
   );
 }
