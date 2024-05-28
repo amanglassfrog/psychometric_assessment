322e1a61b56f453751a0d3737abe5d3e4b564787
amanglassfrog <135016564+amanglassfrog@users.noreply.github.com>
2024-05-28 21:25:51 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,24 @@
 import Header from "@/components/Header/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <head>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-T1RMK1QQK9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+      <Header />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-T1RMK1QQK9"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-  gtag('config', 'G-T1RMK1QQK9');
-</script>
-    </head>
-    <Header />
+            gtag('config', 'G-T1RMK1QQK9');
+          `,
+        }}
+      ></script>
     </>
   );
 }
